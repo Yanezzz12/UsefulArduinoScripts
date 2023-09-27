2f258324dd8154a86e761f66615baef7238f644a
Miguel Yáñez <60891165+Yanezzz12@users.noreply.github.com>
2023-09-27 00:10:25 -0600

    Serial Read
    Reads a string from serial monitor and separates into single commands
    Example: 
    INPUT:  "Hello World!"
    OUTPUT: 
       var1 = "Hello"
       var2 = "World!"

---
+++ SerialRead/SerialRead.ino
@@ -0,0 +1,52 @@
+/*
+Script by Miguel Yanez
+Reads a string in the serial monitor and separates
+every parameter.
+*/
+
+//Variables
+String commandStr;
+String command1;
+String command2;
+String command3;
+  
+void setup() 
+{
+  Serial.begin(9600);
+
+}
+
+String ReadCommands()
+{ 
+  while (Serial.available() == 0) {} 
+  commandStr = Serial.readString();
+  commandStr.trim();  
+
+  int wordCount = 0;
+  for(int i = 0; i < commandStr.length(); i++)
+  {
+    if(commandStr[i] == ' ')      
+      wordCount++;
+    else if(wordCount == 0)
+      command1 += commandStr[i];
+    else if(wordCount == 1)
+      command2 += commandStr[i];
+    else if(wordCount == 2)
+      command3 += commandStr[i];
+  }
+  return command1, command2, command3;  
+}
+
+
+
+
+void loop() 
+{ 
+  command1, command2, command3 = ReadCommands();
+  
+  Serial.println(command1);
+  Serial.println("Space");
+  Serial.println(command2);
+  Serial.println("Space");
+  Serial.println(command3);
+}
