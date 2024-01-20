98731f06ecd09e9b2c7a59d94694d101d12e32c9
Miguel Yáñez <60891165+Yanezzz12@users.noreply.github.com>
2024-01-20 02:38:25 -0600

    Update Practica5-RM.ino

--- Practice05-RM/Practica5-RM.ino
+++ Practice05-RM/Practica5-RM.ino
@@ -4,16 +4,21 @@ GONZALEZ VELASCO OSCAR EDUARDO
 PEREZ YANEZ MIGUEL ANGEL
 */
 
+//Class libraries
+#include "Encoders.h"
+#include "Utilities.h"
+#include "TestSensors.h"
+
 //Predetermined values
-const float preDistance = 10.0f;
-const float preRotation = 90.0f;
+const float baseDistance = 10.0f;
+const float baseAngle = 45.0f;
 //Constants
-#define MOTION Scroll(1000.0f)
-#define FORWARD Scroll(preDistance)
-#define BACK Scroll(-preDistance)
-#define TURNLEFT RotateRobot(preRotation)
-#define TURNRIGHT RotateRobot(-preRotation)
-#define STOP Scroll(0.0f)
+#define MOTION ScrollPID(0.0f)
+#define FORWARD ScrollPID(baseDistance)
+#define BACK ScrollPID(-baseDistance)
+#define TURNLEFT RotatePID(baseAngle)
+#define TURNRIGHT RotatePID(-baseAngle)
+#define STOP MotorMovement("OFF", 0);
 #define MSG Serial.println("Hola mundo!")
 
 //Mathematic values
@@ -35,20 +40,12 @@ const byte rightContactSens = 8;
 const byte currentBridge = 12;
 const byte leftInfraSens = 19;   
 const byte rightInfraSens = 9;
-//Encoder
-const byte rightEncoderA = 2;   //Interruption 1
-const byte leftEncoderA = 3;    //Interruption 2
-const byte leftEncoderB = 4;
-const byte rightEncoderB = 7;
-//LDR sensor
+//LDR sensors
 const byte LDR1 = 14; // Yellow   (A0)
 const byte LDR2 = 15; // White    (A1)
 const byte LDR3 = 16; // Orange   (A2)
 const byte LDR4 = 17; // Green    (A3)
 const byte LDR5 = 18; // Purple   (A4)
-//Interruptions
-volatile long rightCounter;
-volatile long leftCounter;
 //PID Control
 long previousTime[2] = {0, 0};
 float previousError[2] = {0, 0};
@@ -58,7 +55,7 @@ String commandStr;
 String command1;
 String command2;
 String command3;
-
+  
 void setup() 
 { 
   Serial.begin(9600);  
@@ -68,23 +65,6 @@ void setup()
   pinMode(rightMotor, OUTPUT);
   pinMode(vel1, OUTPUT);
   pinMode(vel2, OUTPUT);
-  //Sensors
-  pinMode(currentBridge, OUTPUT);
-  pinMode(leftInfraSens, INPUT); 
-  pinMode(rightInfraSens, INPUT);
-  pinMode(leftContactSens, INPUT_PULLUP);
-  pinMode(rightContactSens, INPUT_PULLUP);
-  //Encoders
-  pinMode(leftEncoderA, INPUT);
-  pinMode(rightEncoderA, INPUT);
-  pinMode(leftEncoderB, INPUT);
-  pinMode(rightEncoderB, INPUT);
-  //LDR sensors
-  pinMode(LDR1, INPUT);
-  pinMode(LDR2, INPUT);
-  pinMode(LDR3, INPUT);
-  pinMode(LDR4, INPUT);
-  pinMode(LDR5, INPUT);
   /*===INITIAL VALUES===*/
   //Turns off motors
   digitalWrite(vel1, LOW);
@@ -93,95 +73,114 @@ void setup()
   digitalWrite(rightMotor, LOW);  
   //Activates logic
   digitalWrite(currentBridge, HIGH);  
-  //Encoder setup
-  attachInterrupt(digitalPinToInterrupt(leftEncoderA), LeftEncoderInterrupt, RISING);
-  attachInterrupt(digitalPinToInterrupt(rightEncoderA), RightEncoderInterrupt, RISING);
+  //Sensors
+  SensorSetup();
+  //Encoder setup & interruptions
+  SetEncodersInput();
 }
 
 void loop()
 {
+  //TestInfrared();
   //TestArrayLDR();
-  //MaxLightValue();
+  //TestContact();
+  //TestEncoders();
+  
   //SerialTestMotors();
+  //MaxLightValue();
+
+  //MotorMovement("L", 60);
+  //MotorMovement("R", 60);
+
+  //ScrollPID(10.0f);
+  //RotatePID(45.0f);
+  
+  //while(true){} //*/
+  
+  //MotorMovement("R", 40);
   //ObstacleAvoidance();
 
-  //ScrollPID(5.0f);
-  //RightPID(100);
-
-  //MotorPID(300, 'R');
-  //MotorMovement("R", 40);
-
-  //TestArrayLDR();
-  //TestEncoders();
-
+  //FORWARD;
+  //while(true){}
+  
   /*/ 
   Scroll(0.0f); RotateRobot(90.0f);
   while(true) {} 
   // */
 }
 
-void MotorPID(long target, char symbol)
+float du = 0.0f;
+bool MotorPID(long target, char symbol)
 {
-  //Control boundaries
-  const float tolerance = 6.0f;
-  const float limitVelocity = 60.0f;
-  const float minVelocity = 20.0f;
-  
-  //Control constants
-  const float Kp = 5.9f;    // Kp = 6.0f
-  const float Ki = 0.01f;   // Ki = 
-  const float Kd = 0.7f;    // Kd = 0.003f
-
   //Variable declaration 
   const short motorQuantity = 2;
   long currentPosition[motorQuantity];
   long currentTime[motorQuantity];
   float dt[motorQuantity];
-  short error[motorQuantity];
+  int error[motorQuantity];
   float Dedt[motorQuantity];
   float uFunction[motorQuantity];
   long destiny[motorQuantity];
+  bool taskCompleted = true;
+  const float limitVelocity[motorQuantity] = {67.0f, 49.0f}; //69 60
+  const float minVelocity[motorQuantity] = {19.f, 21.0f};
+  
+  //Control constants
+  const float Kp[motorQuantity] = {14.0f, 14.0f};   
+  const float Ki[motorQuantity] = {0.004f, 0.004f};  
+  const float Kd[motorQuantity] = {0.025f, 0.025f};  
+
+  //Control boundaries
+  const float tolerance = 4.0f;
 
   short motor;
   if(symbol == 'L')
   {
     motor = 0; 
-    currentPosition[motor] = leftCounter; //Time difference
+    currentPosition[motor] = LeftCount(); //Time difference
   }
   else if(symbol == 'R')
   {
     motor = 1;
-    currentPosition[motor] = rightCounter; //Time difference
+    currentPosition[motor] = RightCount(); //Time difference
   }
 
   destiny[motor] = target;
   currentTime[motor] = micros();
   dt[motor] = ((float)(currentTime[motor] - previousTime[motor]))/(1.0e6);
   previousTime[motor] = currentTime[motor];
-  error[motor] = currentPosition[motor] - destiny[motor]; //Check target too
+  error[motor] = currentPosition[motor] - destiny[motor] - (int)du;
   
   //Derivative & Integral
   Dedt[motor] = (error[motor] - previousError[motor])/(dt[motor]);
-  Iedt[motor] = Iedt[motor] + (error[motor] * dt[motor]); //Convert into array
+  Iedt[motor] = Iedt[motor] + (error[motor] * dt[motor]);
 
   //PID Control Signal
-  uFunction[motor] = (Kp * error[motor]) + (Kd * Dedt[motor]) + (Ki * Iedt[motor]);
-  //uFunction = ErrorToPWM(target, uFunction);
+  uFunction[motor] = (Kp[motor] * error[motor]) + (Kd[motor] * Dedt[motor]) + (Ki[motor] * Iedt[motor]);
+  uFunction[motor] = ErrorToPWM(target, uFunction[motor]);
 
   //Signal processing & Plant
-  if(uFunction[motor] > limitVelocity)
-    uFunction[motor] = limitVelocity;
-  else if(uFunction[motor] < -limitVelocity)
-    uFunction[motor] = -limitVelocity;
-  else if((uFunction[motor] < minVelocity) && (uFunction[motor] > 0))
-    uFunction[motor] = minVelocity;
-  else if((uFunction[motor] < 0) && (uFunction[motor] > -minVelocity)) 
-    uFunction[motor] = -minVelocity;
+  if(uFunction[motor] > limitVelocity[motor])
+    uFunction[motor] = limitVelocity[motor];
+  else if((uFunction[motor] < minVelocity[motor]) && (uFunction[motor] >= 0))
+    uFunction[motor] = minVelocity[motor];
+  else if((uFunction[motor] < 0) && (uFunction[motor] > -minVelocity[motor])) 
+    uFunction[motor] = -minVelocity[motor];
+  else if(uFunction[motor] < -limitVelocity[motor])
+    uFunction[motor] = -limitVelocity[motor];
   
   previousError[motor] = error[motor];
 
+  float delta = 0.01f; //-0.03
+  if(motor == 1)
+    du = ErrorToPWM(target, delta * (uFunction[0] - uFunction[1])) - minVelocity[0];
+
+  if(target < 0)
+    uFunction[motor] *= -1;
+
   if(abs(error[motor]) > tolerance)
   {
+    taskCompleted = false;
     if(symbol == 'L')
       MotorMovement("L", uFunction[motor]);
     else if(symbol == 'R')
@@ -193,29 +192,172 @@ void MotorPID(long target, char symbol)
       MotorMovement("LOFF", 0);
     else if(symbol == 'R')
       MotorMovement("ROFF", 0);
+    taskCompleted == true;
   }
-  //END of plant
+  return taskCompleted;
+}
 
-  //Graphing tool in Serial Plotter
-  Serial.print(abs(target));          Serial.print(", ");
-  Serial.print(abs(leftCounter));   Serial.print(", "); 
-  Serial.println(abs(rightCounter));    
+void PlotPID(long ticks)
+{
+  Serial.print("Target:");      Serial.print(ticks);         Serial.print(",");
+  Serial.print("LeftWheel:");   Serial.print(LeftCount());   Serial.print(",");
+  Serial.print("RightWheel:");  Serial.print(RightCount());  Serial.println(","); 
 }
 
 void ScrollPID(float distance)
 {
   long ticks = DistanceToTicks(distance);
-  MotorPID(ticks, 'L');
-  MotorPID(ticks, 'R');
+  unsigned long startTime;
+  unsigned long currTime = 0;
+  unsigned long settlingTime = 1000; //1 segundo
+
+  while(true)
+  {
+    MotorPID(ticks, 'L');
+    MotorPID(ticks, 'R');
+    if(MotorPID(ticks, 'L') && MotorPID(ticks, 'R'))
+    {
+      startTime = millis();
+      while(currTime < startTime + settlingTime)
+      {
+        currTime = millis();
+        MotorPID(ticks, 'L');
+        MotorPID(ticks, 'R');
+        PlotPID(ticks);
+      }
+      MotorMovement("LOFF", 0);
+      MotorMovement("ROFF", 0); 
+      break; 
+    }
+    PlotPID(ticks);
+  } // */
+}
+
+void RotatePID(float angle)
+{
+  long ticks = AngleToTicks(angle);
+  unsigned long startTime;
+  unsigned long currTime = 0;
+  unsigned long settlingTime = 1000; //1 segundo
+
+  while(true)
+  {
+    MotorPID(-ticks, 'L');
+    MotorPID(ticks, 'R');
+    if(MotorPID(-ticks, 'L') && MotorPID(ticks, 'R'))
+    {
+      startTime = millis();
+      while(currTime < startTime + settlingTime)
+      {
+        currTime = millis();
+        MotorPID(-ticks, 'L');
+        MotorPID(ticks, 'R');
+        PlotPID(ticks);
+      }
+      break; 
+    }
+    PlotPID(ticks);
+  } // */
+}
+
+void LightFollowerAlgorithm()
+{
+  byte direction = MaxLightSensor();
+  int MaxLightValue = 400; //Bias 
+
+  switch(direction) //Modificar casos
+  {
+    case 0: //Luz enfrente
+      //ScrollPID(); //Move indefinetely
+      break;
+    case 1: //Luz a la izquierda
+      //RotatePID(); //Hasta que el mayor valor sea el del LDR de enfrente
+      break;
+    case 2: //Luz a la derecha
+      //RotatePID(); //Hasta que el mayor valor sea el del LDR de enfrente
+      break;
+    case 3:
+      //RotatePID(); //Hasta que el mayor valor sea el del LDR de enfrente
+      break; //Luz detrás
+  }
+
+  if(LDRvalue[direction] > MaxLightValue) //Check if the position is valid
+    STOP;
+}
+
+void Scroll(float distance)
+{
+  //This parameters shows a 10 [cm] scroll
+  const float baseDistance = 10.0f;
+  const float baseTime = 700.0f;
+  const int deviation = 50;
+  const int leftVel = 45;
+  const int rightVel = 38; //54
+  
+  float redefine = abs(distance) / baseDistance;
+
+  if(distance > 999)
+  {
+    MotorMovement("L", rightVel);
+    MotorMovement("R", -leftVel);
+  }
+  else if(distance > 0)
+  {
+    MotorMovement("L", leftVel);
+    MotorMovement("R", -rightVel);
+    delay(int(baseTime * redefine) + deviation);
+    MotorMovement("OFF", 0);
+  }
+  else if(distance < 0) 
+  {
+    MotorMovement("L", -leftVel);
+    MotorMovement("R", rightVel);
+    delay(int(baseTime * redefine) + deviation);
+    MotorMovement("OFF", 0);
+  }
+  else if(distance == 0)
+  {
+    MotorMovement("OFF", 0);
+  }
+  else { }
+}
+
+void RotateRobot(float angle) //Test function
+{ 
+  //This parameters show a 90 degree rotation
+  const float baseRotation = 90.0f;
+  const float baseTime = 800.0f;
+  const int deviation = 50;
+  const int leftVel = 40;
+  const int rightVel = 40;
+  
+  float redefine = abs(angle) / baseRotation;
+
+  if(angle > 0)
+  {
+    MotorMovement("L", rightVel);
+    MotorMovement("R", leftVel);
+    delay(int(baseTime * redefine) + deviation);
+    MotorMovement("OFF", 0);
+  }
+  else if(angle < 0)
+  {
+    MotorMovement("L", -rightVel);
+    MotorMovement("R", -leftVel);
+    delay(int(baseTime * redefine) + deviation);
+    MotorMovement("OFF", 0);
+  }
+  else{}
 }
 
 void MotorMovement(String command, int speedPWM) 
 {
   //Allows motor movement, SpeedPWM: Range[-127, 127]
+  /*
   Serial.print("Command: ");
   Serial.print(command);
   Serial.print(", ");
-  Serial.println(speedPWM);
+  Serial.println(speedPWM); // */
 
   if(command == "L" || command == "R") //Can be tweaked yet
   {
@@ -263,7 +405,7 @@ void MotorMovement(String command, int speedPWM)
 }
 
 String ReadCommands()
-{ 
+{
   while (Serial.available() == 0) {} 
   commandStr = Serial.readString();
   commandStr.trim();  
@@ -284,23 +426,7 @@ String ReadCommands()
   return command1, command2;  
 }
 
-bool InfraredSensor(char sensor) 
-{
-  //Reads an infrared sensor and returns a boolean value if exceeds sensitivity
-  float infraValue, sensitivity = 400.0f;
-  
-  if(sensor == 'L')
-    infraValue = analogRead(leftInfraSens);
-  else if(sensor == 'R')
-    infraValue = analogRead(rightInfraSens);
-
-  if(infraValue > sensitivity)
-    return 0;
-  else 
-    return 1;
-}
-
-byte MaxLightValue()
+byte MaxLightSensor()
 {
   //Detects LDR with higher value and returns an index
   const byte sensorQuantity = 5;
@@ -319,82 +445,6 @@ byte MaxLightValue()
   Serial.println(maxLightIndex);
   // */
   return maxLightIndex; 
-}
-
-bool ContactSensor(char sensor)
-{
-  //Reads a contact sensor and returns a boolean value
-  bool registeredValue;
-
-  if(sensor == 'L')
-    registeredValue = digitalRead(leftContactSens);
-  else if(sensor == 'R')
-    registeredValue = digitalRead(rightContactSens);
-  return registeredValue; 
-}
-
-void RotateRobot(float angle)
-{ 
-  //Rotates robot a given angle (Not very precise)
-  const float baseRotation = 90.0f;
-  const float baseTime = 900.0f;
-  const int deviation = 50;
-  int leftVel = 40;
-  int rightVel = 40;
-  
-  float redefine = abs(angle) / baseRotation;
-
-  if(angle < 0)
-  {
-    leftVel *= -1;
-    rightVel *= -1;
-  }
-
-  MotorMovement("R", rightVel);
-  MotorMovement("L", leftVel);
-  delay(int(baseTime * redefine) + deviation);
-  MotorMovement("OFF", 0);
-}
-
-void Scroll(float distance)
-{
-  //Allows linear movement in a given distance in [cm] (Not very precise)
-  const float baseDistance = 10.0f;
-  const float baseTime = 700.0f;
-  const int deviation = 50;
-  const int leftVel = 45;
-  const int rightVel = 38; 
-  
-  float redefine = abs(distance) / baseDistance;
-
-  if(distance > 999)
-  {
-    MotorMovement("L", -leftVel);
-    MotorMovement("R", rightVel);
-  }
-  else if(distance > 0)
-  {
-    MotorMovement("L", -leftVel);
-    MotorMovement("R", rightVel);
-    delay(int(baseTime * redefine) + deviation);
-    MotorMovement("OFF", 0);
-  }
-  else if(distance < 0) 
-  {
-    MotorMovement("L", leftVel);
-    MotorMovement("R", -rightVel);
-    delay(int(baseTime * redefine) + deviation);
-    MotorMovement("OFF", 0);
-  }
-  else if(distance == 0)
-    MotorMovement("OFF", 0);
-}
-
-void MoveRobot(float angle, float distance)
-{
-  //First: Move to angle, then move distance
-  RotateRobot(angle);
-  Scroll(distance);
 }
 
 void ObstacleAvoidance() //Obstacle avoidance algorithm 
@@ -419,7 +469,7 @@ void ObstacleAvoidance() //Obstacle avoidance algorithm
   switch(state)
   {
     case 0: //No obstacle in front
-      MOTION;
+      LightFollowerAlgorithm();
       break;
     case 1: //Obstacle to the right
       STOP;
@@ -442,125 +492,8 @@ void ObstacleAvoidance() //Obstacle avoidance algorithm
  }
 }
 
-void LeftEncoderInterrupt()
-{
-  if(digitalRead(leftEncoderA) == HIGH)
-    if(digitalRead(leftEncoderB) == LOW)
-      leftCounter--;
-    else
-      leftCounter++;
-  else
-    if(digitalRead(leftEncoderB) == LOW)
-      leftCounter++;
-    else
-      leftCounter--;
-}
-
-void RightEncoderInterrupt()
-{
-  if(digitalRead(rightEncoderA) == HIGH)
-    if(digitalRead(rightEncoderB) == LOW)
-      rightCounter++;
-    else
-      rightCounter--;
-  else
-    if(digitalRead(rightEncoderB) == LOW)
-      rightCounter--;
-    else
-      rightCounter++;
-}
-
-void TestContact()
-{
-  Serial.print("Left: ");   Serial.println(ContactSensor('L'));
-  Serial.print("Right: ");  Serial.println(ContactSensor('R'));
-  delay(1000);
-}
-
-void TestInfrared()
-{
-  Serial.print("Lectura Izq: "); 
-  Serial.print(analogRead(leftInfraSens)); 
-  //Serial.println(InfraredSensor('L'));
-
-  Serial.print(", Lectura Derecha: "); 
-  Serial.print(analogRead(rightInfraSens)); 
-  //Serial.println(InfraredSensor('R'));
-
-  delay(600);
-}
-
-void TestEncoders()
-{
-  Serial.print("Left: ");
-  Serial.print(leftCounter);
-  Serial.print(", Right: ");
-  Serial.println(rightCounter);
-  delay(100);
-}
-
-void TestArrayLDR()
-{
-  const byte sensorQuantity = 5;
-  unsigned int LDRvalue[sensorQuantity];
-  
-  for(byte i = 0; i < sensorQuantity; i++)
-    LDRvalue[i] = analogRead(i + 14);
-  
-  Serial.print("Valores: ");
-  for(byte i = 0; i < sensorQuantity; i++)
-  {
-    Serial.print(LDRvalue[i]);
-    Serial.print(", ");
-  }
-  Serial.println(" ");
-  delay(1000);
-}
-
 void SerialTestMotors()
 {
   command1, command2 = ReadCommands();
   MotorMovement(command1, command2.toInt());
-}
-
-short Map(short value) 
-{
-  //Input: Range[-127, 127], Output: Range[0, 255]
-  value += 127;
-  return value;
-}
-
-short AngleCorrection(short angle)
-{  
-  while(angle > 180)
-    angle -= 360;
-  while(angle < 180)
-    angle += 360;
-  return angle;
-}
-
-float ErrorToPWM(long maxError, float currentError)
-{
-  /* This function transforms an e(t) to a F(s) signal able to get in the plant (Motor)
-   * The mathematic model is linear, described by m = (maxPWMValue - minPWMValue) / (maxError - minError)
-   * m = (85 - 19) / (maxError - 0) => m = 66/target => F(s) = [66*e(t)]/target + minPWMValue
-  */
-  currentError = (66.0f * currentError)/maxError + 20.0f;
-  return currentError;
-}
-
-long DistanceToTicks(float distance)
-{
-  long ticks = 22.206f * distance;
-  return ticks;
-}
-
-volatile long LeftTickValue()
-{
-  return leftCounter;
-}
-
-volatile long RightTickValue()
-{
-  return rightCounter;
 }
